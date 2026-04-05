d890fc0568bee86c310de48695bf199c01962505
Anshul Sharma <ianshulsharma26@gmail.com>
2026-04-05 08:37:07 +0530

    add routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App'
+import About from './Components/About'
+import Contact from './Components/Contact'
+import Home from './Components/Home'
+
+import { createBrowserRouter,RouterProvider } from 'react-router'
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={routes} />
 )
